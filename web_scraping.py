6242cae169fd16c55d4fd9950be70aa3fb4f2e71
DSGuthrie <david.s.guthrie@gmail.com>
2019-03-28 03:29:33 -0400

    Created function to download site content and added error handling

--- web_scraping.py
+++ web_scraping.py
@@ -1,22 +1,37 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Web Scrapper
 @author: David Guthrie
 """
+# TODO: Add support for multiple pages
+#		Research using robots.txt and sitemap to navigate site
 
 from bs4 import BeautifulSoup as bSoup
 from urllib.request import urlopen
+from urllib.error import URLError, HTTPError, ContentTooShortError
 
-# Site to target for scraping
+# TODO: Update to read target site from a file
 targetSite = "http://books.toscrape.com/"
 
-# Connet to site, get site conntent, disconnect
-siteConnection = urlopen(targetSite)
-site_html = siteConnection.read()
-siteConnection.close()
+site_html = downloadSite(targetSite)
 
 site_soup = bSoup(site_html, "html.parser")
 
-# TODO: Add support for multiple pages
-#		Research using sitemap to navigate site
+# TODO: Update so these parameters are read from a file
+containers = site_soup.find_all("article",{"class":"product_pod"})
+
+
+# Connet to site, get site conntent, disconnect
+def downloadSite(url):
+    print('Downloading:', url)
+    try:
+		siteConnection = urlopen(url)
+        html = siteConnection.read()
+    except (URLError, HTTPError, ContentTooShortError) as e:
+        print('Download error:', e.reason)
+        html = None
+	finally:
+		siteConnection.close()
+    return html
+
